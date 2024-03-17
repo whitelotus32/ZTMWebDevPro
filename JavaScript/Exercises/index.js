53de1ea453475bac0f97463c75defe293ec0accb
Nicholas Dixon <NickDixon32@proton.me>
2024-03-17 14:13:47 -0500

    Added solutions for the exercise

--- JavaScript/Exercises/index.js
+++ JavaScript/Exercises/index.js
@@ -1,6 +1,11 @@
 // add variable "firstName" and "lastName" // so that they equal your name
 
+var firstName = "Nick";
+var lastName = "Dixon";
+
 // create a variable that holds the answer // of "firstName" + " " + "lastName"
+
+var fullName = firstName + " " + lastName;
 
 // Evaluate this question. What is a + b?
 var a = 34;
@@ -8,6 +13,9 @@ var b = 21;
 a = 2;
 a + b // what is the answer here?
 
+// Answer = 23
+
 // What is c equal to?
 var c;
 
+// c = undefined
