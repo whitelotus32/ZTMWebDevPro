eda6f4308b6e8a48ee5a2ca77ff1b042d7cc0e98
Nicholas Dixon <NickDixon32@proton.me>
2024-03-19 22:56:37 -0500

    Added the solutions to the exercise

--- JavaScript/Exercises/index.js
+++ JavaScript/Exercises/index.js
@@ -112,17 +112,21 @@ function checkDriverAge(age) {
 // using this array,
 // var array = ["Banana", "Apples", "Oranges", "Blueberries"];
 
-
 // 1. Remove the Banana from the array.
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
 
 // 2. Sort the array in order.
+array.shift();
 
 // 3. Put "Kiwi" at the end of the array.
+array.push("Kiwi");
 
 // 4. Remove "Apples" from the array.
+array.splice(0, 1);
 
 // 5. Sort the array in reverse order. (Not alphabetical, but reverse
 // the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
+array.reverse();
 
 // you should have at the end:
 // ["Kiwi", "Oranges", "Blueberries"]
@@ -130,3 +134,4 @@ function checkDriverAge(age) {
 // using this array,
 // var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 // access "Oranges".
+array2[1][1][0];
