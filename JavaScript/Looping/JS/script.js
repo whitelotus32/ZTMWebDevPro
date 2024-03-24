6eec8adb059e80d4e1e9b0697386aa0f99d20879
Nicholas Dixon <NickDixon32@proton.me>
2024-03-24 15:39:00 -0700

    Created an infinity loop.

--- JavaScript/Looping/JS/script.js
+++ JavaScript/Looping/JS/script.js
@@ -5,3 +5,14 @@ var todos = [
     "study javascript!",
     "eat healthy!"
 ];
+
+var todosLength = todos.length;
+// for (var i=0; i < todos.length; i++) {
+//     todos.pop();
+// }
+
+var counterOne = 10;
+while (counterOne > 0) {
+    console.log(counterOne);
+    counterOne++;
+}
