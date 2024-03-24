d91f0571634b4717c1486f0764836209bcf58101
Nicholas Dixon <NickDixon32@proton.me>
2024-03-23 19:15:00 -0700

    Created file structure for the Facebook app and created two variables

---
+++ JavaScript/Facebook/JS/index.js
@@ -0,0 +1,17 @@
+var database = [
+    {
+        username: "Nick",
+        password: "sonGoku"
+    }
+];
+
+var newsFeed = [
+    {
+        username: "Jordyn",
+        timeline: "So tired from all that pizza!"
+    },
+    {
+        username: "JupiPoopy",
+        timeline: "I like to eat pizza upside down"
+    }
+];
