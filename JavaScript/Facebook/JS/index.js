c4063104a1273c7add407193583aab680041e69e
Nicholas Dixon <NickDixon32@proton.me>
2024-03-23 19:39:35 -0700

    Created the sign in function

--- JavaScript/Facebook/JS/index.js
+++ JavaScript/Facebook/JS/index.js
@@ -18,3 +18,13 @@ var newsFeed = [
 
 var userNamePrompt = prompt("What's your username?");
 var passwordPrompt = prompt("What's your password?");
+
+function signIn(username, password) {
+    if (username === database[0].username && password === database[0].password) {
+        console.log(newsFeed);
+    }   else {
+            alert("Sorry, wrong username and password!");
+        }
+}
+
+signIn(userNamePrompt, passwordPrompt);
