88372f53f4bf4ae8d303dd45da7093bd6c824ddd
Nicholas Dixon <NickDixon32@proton.me>
2024-03-29 12:30:43 -0700

    Added event listeners for both colors.

--- JavaScript/BackgroundGenerator/JS/script.js
+++ JavaScript/BackgroundGenerator/JS/script.js
@@ -1,3 +1,11 @@
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+
+color1.addEventListener("input", function(){
+    console.log(color1.value);
+})
+
+color2.addEventListener("input", function(){
+    console.log(color2.value);
+})
