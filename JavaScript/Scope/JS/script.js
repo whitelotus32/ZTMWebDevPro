4e3841f5f50646240027cc71f9bd1df884828d76
Nicholas Dixon <NickDixon32@proton.me>
2024-04-03 17:52:43 -0700

    Added the solutions to the Scope exercise.

--- JavaScript/Scope/JS/script.js
+++ JavaScript/Scope/JS/script.js
@@ -5,7 +5,7 @@ function q1() {
     if(a > 1) {
         a = 3;
     }
-    alert(a);
+    alert(a); //3
 }
 
 //#2
@@ -15,8 +15,12 @@ function q2() {
 }
 
 function q22() {
-    alert(a);
+    alert(a); //5
 }
+// run in the console:
+// q22() -- 0
+// q2() -- undefined
+// q22() -- 5
 
 
 //#3
@@ -24,25 +28,25 @@ function q3() {
     window.a = "hello";
 }
 
-
+// first run q3() in the console to add the a property to the window. then run q32()
 function q32() {
-    alert(a);
+    alert(a); //hello
 }
 
 //#4
 var a = 1;
 function q4() {
     var a = "test";
-    alert(a);
+    alert(a); //"test"
 }
 
 //#5
 var a = 2;
 if (true) {
     var a = 5;
-    alert(a);
+    alert(a); //5
 }
-alert(a);
+alert(a); //5
 
 
 
