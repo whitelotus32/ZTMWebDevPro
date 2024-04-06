868e88ed7109387d0a21c7530d5970cdf1acaf68
Nicholas Dixon <NickDixon32@proton.me>
2024-04-05 17:12:29 -0700

    Added the solutions to the ES5&ES6 exercise.

--- JavaScript/Exercises/ES5&ES6/JS/script.js
+++ JavaScript/Exercises/ES5&ES6/JS/script.js
@@ -1,19 +1,20 @@
 // change everything below to the newer Javascript!
 
 // let + const
-var a = 'test';
-var b = true;
-var c = 789;
+let a = 'test';
+const b = true;
+const c = 789;
 a = 'test2';
 
 
 // Destructuring
-var person = {
+const person = {
     firstName : "John",
     lastName  : "Doe",
     age       : 50,
     eyeColor  : "blue"
 };
+// const { firstName, lastName, age, eyeColor } = person;
 
 var firstName = person.firstName;
 var lastName = person.lastName;
@@ -22,32 +23,27 @@ var eyeColor = person.eyeColor;
 
 
 // Object properties
-var a = 'test';
-var b = true;
-var c = 789;
+const a = 'test';
+const b = true;
+const c = 789;
 
-var okObj = {
-  a: a,
-  b: b,
-  c: c
-};
+const okObj = {a, b, c};
 
 
 // Template strings
-var message = "Hello " + firstName + " have I met you before? I think we met in " + city + " last summer no???";
+const message = `Hello  + ${firstName} have I met you before? I think we met in ${city} last summer no???`;
 
 
 // default arguments
 // default age to 10;
-function isValidAge(age) {
-    return age
-}
+const isValidAge = (age = 10) => age;
 
 // Symbol
 // Create a symbol: "This is my first Symbol"
+const sym = Symbol('This is my first Symbol');
 
 // Arrow functions
-function whereAmI(username, location) {
+const whereAmI = (username, location) => {
     if (username && location) {
         return "I am not lost";
     } else {
