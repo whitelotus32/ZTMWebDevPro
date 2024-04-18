792ae03914d6b7bb983d784f8b01527a9c862a01
Nicholas Dixon <NickDixon32@proton.me>
2024-04-18 15:41:27 -0700

    Dummy commit: Forgot to add the exercises.

---
+++ JavaScript/Promises/index.js
@@ -0,0 +1,16 @@
+// Solve the questions below:
+
+// #1) Create a promise that resolves in 4 seconds and returns "success" string
+
+// #2) Run the above promise and make it console.log "success"
+
+// #3) Read about Promise.resolve() and Promise.reject(). How can you make
+// the above promise shorter with Promise.resolve() and console loggin "success"
+
+// #4) Catch this error and console log 'Ooops something went wrong'
+
+// #5) Use Promise.all to fetch all of these people from Star Wars (SWAPI) at the same time.
+// Console.log the output and make sure it has a catch block as well.
+
+// #6) Change one of your urls above to make it incorrect and fail the promise
+// does your catch block handle it?
